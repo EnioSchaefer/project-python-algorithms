6c0555161ca26153d21adcb3e18a9416b823509b
Enio Schaefer <enio100schaefer@hotmail.com>
2023-06-02 15:51:28 -0300

    feat: req 1 concluido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+
+    period_counter = 0
+    for student in permanence_period:
+        start_study = student[0]
+        end_study = student[1]
+
+        if type(start_study) != int or type(end_study) != int:
+            return None
+
+        period = range(start_study, (end_study + 1))
+
+        if target_time in period:
+            period_counter += 1
+
+    return period_counter
