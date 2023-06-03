eb4697fbad2a0feb7800eca9b699525e7f440acd
Enio Schaefer <enio100schaefer@hotmail.com>
2023-06-03 03:18:28 -0300

    feat: req 2 concluido

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(message="abcabc", key="abc")
+
+    with pytest.raises(TypeError):
+        encrypt_message(message=123123, key=123)
+
+    assert encrypt_message(message="random", key=999) == "modnar"
+
+    assert encrypt_message(message="random", key=3) == "nar_mod"
+
+    assert encrypt_message(message="random", key=2) == "modn_ar"
